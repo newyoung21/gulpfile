73d8a13c18952f9ad9cc3e8f83d7a82758078745
newyoung21 <854795800@qq.com>
2017-01-12 12:04:20 +0800

    sd

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
-## 所有依赖安装完之后，在命令行直接敲
-    
-    gulp default
-    就可以自动化运行了
-    
-### 下面是 gulpfile.js文件 的所有内容 
 'use strict';
-##加载 gulp 插件
+//加载 gulp 插件
 var gulp =  require('gulp'),
     cssnano = require('gulp-cssnano'),//css压缩
     concat = require('gulp-concat'),//文件合并
@@ -14,11 +8,11 @@ var gulp =  require('gulp'),
     runSequence = require('run-sequence'),//gulp 同步执行
     browserSync = require('browser-sync').create();//浏览器同步更新
 
-  ##默认gulp 
-  ###第一步清空文件 clear
-  ###第二步构建文件 build
-  ###第三步开启服务和监听文件 ['serve','watch']
-    
+    /*默认gulp 
+      第一步清空文件 clear
+      第二步构建文件 build
+      第三步开启服务和监听文件 ['serve','watch']
+    */
     gulp.task('default',function(callback){
       return runSequence('clear','build',['serve','watch'],callback);
     });
@@ -26,7 +20,7 @@ var gulp =  require('gulp'),
     gulp.task('clear',function(callback){
       return del(['dist/'], callback);
     });
-###第二步 同步执行 copy(文件复制) -> minijs(js混淆) -> minicss(css压缩) 按顺序来。
+    //第二步 同步执行 copy(文件复制) -> minijs(js混淆) -> minicss(css压缩) 按顺序来。
     gulp.task('build',function(callback){
       return runSequence('copy','miniJs','minicss',callback);
     });
@@ -44,7 +38,7 @@ var gulp =  require('gulp'),
         .pipe(cssnano())
         .pipe(gulp.dest('dist/css/'));
     });
-###开启本地服务器
+    //开启本地服务器
     gulp.task('serve',function(){
        browserSync.init({
         server:{
@@ -52,7 +46,7 @@ var gulp =  require('gulp'),
         }
       })
     });
-###监听文件变化后执行reload
+    //监听文件变化后执行reload
     gulp.task('watch',function(){
        gulp.watch('src/**/*.*',['reload']);
     });
